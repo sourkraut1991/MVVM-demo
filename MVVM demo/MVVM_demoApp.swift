5a4f8d59966289e8aa06382a7de61959f8680314
sourkraut1991 <69095925+sourkraut1991@users.noreply.github.com>
2022-11-15 06:10:28 +0300

    Initial Commit

---
+++ MVVM demo/MVVM_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_demoApp.swift
+//  MVVM demo
+//
+//  Created by ed on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
